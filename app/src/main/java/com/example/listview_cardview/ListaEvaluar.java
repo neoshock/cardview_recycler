7abd8693ab2e2345373836093e011610cb7f4e60
neoshock <pideun@gmail.com>
2021-12-17 17:36:27 -0500

    Trabajo Completado.

---
+++ app/src/main/java/com/example/listview_cardview/ListaEvaluar.java
@@ -0,0 +1,19 @@
+package com.example.listview_cardview;
+
+import java.util.List;
+
+public class ListaEvaluar {
+    List<Evaluado> listaaevaluar;
+
+    public ListaEvaluar(List<Evaluado> listaaevaluar) {
+        this.listaaevaluar = listaaevaluar;
+    }
+
+    public List<Evaluado> getListaaevaluar() {
+        return listaaevaluar;
+    }
+
+    public void setListaaevaluar(List<Evaluado> listaaevaluar) {
+        this.listaaevaluar = listaaevaluar;
+    }
+}
